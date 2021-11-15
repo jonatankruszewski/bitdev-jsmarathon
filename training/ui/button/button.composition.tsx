9963a20521356fe51a858e45230ebb00727dcabc
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-11-15 11:21:40 -0300

    Added Button compositions

--- training/ui/button/button.composition.tsx
+++ training/ui/button/button.composition.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { Button } from './button';
+import React from "react";
+import { Button } from "./button";
 
-export const BasicButton = () => (
-  <Button text="hello from Button" />
+export const PrimaryButton = () => (
+  <Button importance='primary' text='hello from Button' />
+);
+
+export const SecondaryButton = () => (
+  <Button importance='secondary' text='hello from Button' />
 );
