ab9251836edcdd2782db70662ac804ee71c2d30f
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-11-15 11:27:48 -0300

    Added Card component

---
+++ training/ui/card/card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./card.module.scss";
+import { Button } from "@jkru/ui.button";
+
+export type CardProps = {
+  /**
+   * a text to be rendered in the component.
+   */
+  text: string;
+};
+
+export function Card({ text }: CardProps) {
+  return (
+    <div className={styles.card}>
+      <h2>{text}</h2>
+      <Button text='Click me'></Button>
+    </div>
+  );
+}
